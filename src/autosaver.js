f7c1ebbfc75056aadc7ce5a51b7dde32ce3d5380
Coding <dev.tandpfun@gmail.com>
2021-08-27 17:32:53 -0700

    Initial commit

---
+++ src/autosaver.js
@@ -0,0 +1,4 @@
+console.log('Auto Saver Initialized!')
+console.log(window.location.href)
+
+//setInterval(() => document.dispatchEvent(new KeyboardEvent('keydown',  {'key':'s', metaKey: true})), 10000)
