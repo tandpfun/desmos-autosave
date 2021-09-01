e8fe7c6a5cc17a046e687a2c98c4867d4f855270
Coding <dev.tandpfun@gmail.com>
2021-09-01 10:23:25 -0700

    check if signed in, style fixes

--- src/autosaver.js
+++ src/autosaver.js
@@ -4,65 +4,68 @@ const isApple = () => navigator.platform.match(/(Mac|iPhone|iPod|iPad)/i);
 let saveTimeout;
 
 function saveDocument({ autoSaveInterval, autoSaveEnabled }) {
-	if (regex.test(window.location.href)) {
-		if (autoSaveEnabled) {
-			if (isApple())
-				document.dispatchEvent(
-					new KeyboardEvent("keydown", { key: "s", metaKey: true })
-				);
-			else
-				document.dispatchEvent(
-					new KeyboardEvent("keydown", { key: "s", ctrlKey: true })
-				);
+  if (
+    regex.test(window.location.href) &&
+    !document.getElementsByClassName('dcg-sign-in')?.[0]
+  ) {
+    if (autoSaveEnabled) {
+      if (isApple())
+        document.dispatchEvent(
+          new KeyboardEvent('keydown', { key: 's', metaKey: true })
+        );
+      else
+        document.dispatchEvent(
+          new KeyboardEvent('keydown', { key: 's', ctrlKey: true })
+        );
 
-			console.log(
-				`[Desmos Auto Save] Saved graph using method ${
-					isApple() ? "cmd-s" : "ctrl-s"
-				}. Saving again in ${autoSaveInterval}.`
-			);
-		}
-	}
+      console.log(
+        `[Desmos Auto Save] Saved graph using method ${
+          isApple() ? 'cmd-s' : 'ctrl-s'
+        }. Saving again in ${autoSaveInterval}.`
+      );
+    }
+  } else console.log(`[Desmos Auto Save] Not saving: Not signed in`);
 
-	saveTimeout = setTimeout(() => {
-		chrome.storage.sync.get(
-			["autoSaveInterval", "autoSaveEnabled"],
-			(settingsResponse) => {
-				saveDocument(settingsResponse);
-			}
-		);
-	}, autoSaveInterval || 60000);
+  saveTimeout = setTimeout(() => {
+    chrome.storage.sync.get(
+      ['autoSaveInterval', 'autoSaveEnabled'],
+      (settingsResponse) => {
+        saveDocument(settingsResponse);
+      }
+    );
+  }, autoSaveInterval || 60000);
 }
 
-chrome.storage.sync.get("autoSaveInterval", ({ autoSaveInterval }) => {
-	saveTimeout = setTimeout(() => {
-		chrome.storage.sync.get(
-			["autoSaveInterval", "autoSaveEnabled"],
-			(settingsResponse) => {
-				saveDocument(settingsResponse);
-			}
-		);
-	}, autoSaveInterval || 60000);
+chrome.storage.sync.get('autoSaveInterval', ({ autoSaveInterval }) => {
+  saveTimeout = setTimeout(() => {
+    chrome.storage.sync.get(
+      ['autoSaveInterval', 'autoSaveEnabled'],
+      (settingsResponse) => {
+        saveDocument(settingsResponse);
+      }
+    );
+  }, autoSaveInterval || 60000);
 });
 
 chrome.runtime.onMessage.addListener((message) => {
-	if (
-		message.type === "clearInterval" &&
-		message.data?.newInterval &&
-		saveTimeout
-	) {
-		console.log(
-			`[Desmos Auto Save] Received an update interval message. Setting interval to ${message.data?.newInterval}`
-		);
+  if (
+    message.type === 'clearInterval' &&
+    message.data?.newInterval &&
+    saveTimeout
+  ) {
+    console.log(
+      `[Desmos Auto Save] Received an update interval message. Setting interval to ${message.data?.newInterval}`
+    );
 
-		clearInterval(saveTimeout);
+    clearInterval(saveTimeout);
 
-		saveTimeout = setTimeout(() => {
-			chrome.storage.sync.get(
-				["autoSaveInterval", "autoSaveEnabled"],
-				(settingsResponse) => {
-					saveDocument(settingsResponse);
-				}
-			);
-		}, message.data.newInterval || 60000);
-	}
+    saveTimeout = setTimeout(() => {
+      chrome.storage.sync.get(
+        ['autoSaveInterval', 'autoSaveEnabled'],
+        (settingsResponse) => {
+          saveDocument(settingsResponse);
+        }
+      );
+    }, message.data.newInterval || 60000);
+  }
 });
--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
 chrome.runtime.onInstalled.addListener(() => {
-	chrome.storage.sync.set({ autoSaveEnabled: true, autoSaveInterval: 60000 });
-	console.log(`[Installed] Set default settings and enabled auto save!`);
+  chrome.storage.sync.set({ autoSaveEnabled: true, autoSaveInterval: 60000 });
+  console.log(`[Installed] Set default settings and enabled auto save!`);
 });
